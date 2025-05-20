d422a4ea1662c3711653c39efc4378c66ee93729
Michael Lowenstein <mlowenstein@collegehouse.com>
2025-05-19 23:21:46 -0600

    pre-transfer to desktop

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+// import { AuthModule } from './auth/auth.module';
+// import { EventsModule } from './events/events.module';
+// import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'michael',
+      password: 'eoml',
+      database: 'universe',
+      autoLoadEntities: true,
+      synchronize: true, // turn off in prod.
+    }),
+    EventEmitterModule.forRoot(),
+  ],
+  // AuthModule,
+  // EventsModule,
+  // NotificationsModule,
 })
 export class AppModule {}
